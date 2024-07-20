d76bfffcb098a98efc76082ea3ca08050848232b
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-07-19 23:11:45 -0300

    Feat: exercises training

--- exercicios-react/src/App.jsx
+++ exercicios-react/src/App.jsx
@@ -1,52 +1,21 @@
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
+import React from 'react'
+
+// Os links abaixo puxam dados de um produto em formato JSON
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado 
+
 
 const App = () => {
-  const newProducts = produtos.map(({ nome, preco, cores }) => ({
-    nome: nome,
-    preco: Number(preco.replace("R$ ", "")),
-    cores: cores,
-  }));
-
   return (
-    <section>
-      {newProducts.map(({ nome, preco, cores }) =>
-        preco > 1500 ? (
-          <div key={nome}>
-            <h1>{nome}</h1>
-            <p>R$: {preco}</p>
-            <ul>
-              {cores.map((color) => {
-               return <li style={{background: color}} key={color}>{color}</li>
-              })}
-            </ul>
-          </div>
-        ) : (
-          ""
-        )
-      )}
-    </section>
-  );
-};
+    <div>
+      <h1>Exercicios</h1>
+    </div>
+  )
+}
 
-export default App;
+export default App
