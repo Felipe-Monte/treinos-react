7f68ee8068c48456301871f7000adca4a12cce89
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-07-25 22:07:38 -0300

    Style: organize code

--- exercicios-react/src/App.jsx
+++ exercicios-react/src/App.jsx
@@ -1,20 +1,15 @@
 import React, { useEffect } from "react";
 import Product from "./product";
 
-// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
-// Defina o produto clicado como uma preferência do usuário no localStorage
-// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
-
 const App = () => {
-  const [product, setProduct] = React.useState(localStorage.getItem("preference"));
+  const [product, setProduct] = React.useState(
+    localStorage.getItem("preference")
+  );
   const [preference, setPreference] = React.useState(null);
 
   React.useEffect(() => {
-    setPreference(product)
-  }, [])
+    setPreference(product);
+  }, []);
 
   function handleClick({ target }) {
     let productName = target.textContent;
