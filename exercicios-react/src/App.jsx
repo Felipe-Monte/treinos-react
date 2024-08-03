4741b7eb8beca64aa0be806364065803865ae41e
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-08-02 22:05:11 -0300

    Feat: studying form

--- exercicios-react/src/App.jsx
+++ exercicios-react/src/App.jsx
@@ -1,15 +1,58 @@
 import React from "react";
-import Product from "./product";
 
-import { GlobalStorage } from "./GlobalContext";
-import Clear from "./clear";
+// Faça um fetch (POST) para a API abaixo
+// Para a criação ser aceita é necessário enviar dodos de:
+// nome, email, senha, cep, rua, numero, bairro, cidade e estado
+
+// Essa é a função utilizado para realizar o POST
+// fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   // form é o objeto com os dados do formulário
+//   body: JSON.stringify(form),
+// });
+
+// Mostre uma mensagem na tela, caso a resposta da API seja positiva
 
 const App = () => {
+  const [form, setForm] = React.useState({
+    name: "",
+    email: "",
+    password: "",
+    zipCode: "",
+    road: "",
+    number: "",
+    local: "",
+    city: "",
+    state: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+  }
+
   return (
-    <GlobalStorage>
-      <Product />
-      <Clear/>
-    </GlobalStorage>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Nome</label>
+      <input id="name" type="text" value={form.name} onChange={handleChange} />
+
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        type="text"
+        value={form.email}
+        onChange={handleChange}
+      />
+
+      <button>Enviar</button>
+    </form>
   );
 };
 
