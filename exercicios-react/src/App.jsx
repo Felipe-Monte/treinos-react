2a0e95aa2a8e3f9465c683b94c155b44186e6025
Carlos Felipe <carlosfelipemonte1997@outlook.com>
2024-08-07 22:23:50 -0300

    Feat: training inputs

--- exercicios-react/src/App.jsx
+++ exercicios-react/src/App.jsx
@@ -1,19 +1,5 @@
 import React from "react";
 
-// Faça um fetch (POST) para a API abaixo
-// Para a criação ser aceita é necessário enviar dodos de:
-// nome, email, senha, cep, rua, numero, bairro, cidade e estado
-
-// Essa é a função utilizado para realizar o POST
-// fetch('https://ranekapi.origamid.dev/json/api/usuario', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   // form é o objeto com os dados do formulário
-//   body: JSON.stringify(form),
-// });
-
 // Mostre uma mensagem na tela, caso a resposta da API seja positiva
 
 const App = () => {
@@ -28,20 +14,55 @@ const App = () => {
     city: "",
     state: "",
   });
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+  const [errorMessage, setErrorMessage] = React.useState("");
 
   function handleChange({ target }) {
     const { id, value } = target;
     setForm({ ...form, [id]: value });
   }
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log(form);
+
+    try {
+      const response = await fetch(
+        "https://ranekapi.origamid.dev/json/api/usuario",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        }
+      );
+
+      const data = await response.json(); // Extrair a resposta JSON
+
+      console.log(response);
+
+      if (response.ok) {
+        console.log("Dados cadastrados com sucesso!", data);
+        setErrorMessage("");
+      } else {
+        setErrorMessage(data.message || "Ocorreu um erro"); // Usar a mensagem de erro da API
+      }
+    } catch (error) {
+      console.error("Erro na requisição", error);
+      setErrorMessage("Erro ao conectar com servidor");
+    }
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Nome</label>
-      <input id="name" type="text" value={form.name} onChange={handleChange} />
+      <input
+        id="name"
+        type="text"
+        value={form.name}
+        onChange={handleChange}
+        required
+      />
 
       <label htmlFor="email">Email</label>
       <input
@@ -49,7 +70,73 @@ const App = () => {
         type="text"
         value={form.email}
         onChange={handleChange}
+        required
       />
+
+      <label htmlFor="password">Senha</label>
+      <input
+        id="password"
+        type="password"
+        value={form.password}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="zipCode">Cep</label>
+      <input
+        id="zipCode"
+        type="number"
+        value={form.zipCode}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="road">Rua</label>
+      <input
+        id="road"
+        type="text"
+        value={form.road}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="number">Numero</label>
+      <input
+        id="number"
+        type="number"
+        value={form.number}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="local">Bairro</label>
+      <input
+        id="local"
+        type="text"
+        value={form.local}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="city">Cidade</label>
+      <input
+        id="city"
+        type="text"
+        value={form.city}
+        onChange={handleChange}
+        required
+      />
+
+      <label htmlFor="state">Estado</label>
+      <input
+        id="state"
+        type="text"
+        value={form.state}
+        onChange={handleChange}
+        required
+      />
+
+      {errorMessage && <p>{errorMessage}</p>}
 
       <button>Enviar</button>
     </form>
